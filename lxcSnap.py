868dd09eb4f79cde069a86178e260c1f7274fe8a
Neoon <neo@tiefkuehler-madness.me>
2024-06-25 16:45:44 +0200

    update cli

--- lxcSnap.py
+++ lxcSnap.py
@@ -7,21 +7,21 @@ import sys, os
 path = os.path.dirname(os.path.realpath(__file__))
 snap = SNAP(path)
 
-if len(sys.argv) == 1:
-    print("Missing argument")
-elif sys.argv[1] == "update":
+if sys.argv[1] == "update":
     snap.update()
-elif sys.argv[1] == "create":
+elif sys.argv[1] == "create" and len(sys.argv) > 2:
     snap.create(sys.argv[2:])
-elif sys.argv[1] == "list":
+elif sys.argv[1] == "list" and len(sys.argv) > 2:
     snap.backupsList(sys.argv[2])
-elif sys.argv[1] == "download":
+elif sys.argv[1] == "download" and len(sys.argv) > 2:
     snap.download(sys.argv[2])
-elif sys.argv[1] == "delete":
+elif sys.argv[1] == "delete" and len(sys.argv) > 2:
     snap.delete(sys.argv[2])
-elif sys.argv[1] == "restore":
+elif sys.argv[1] == "restore" and len(sys.argv) > 2:
     snap.restore(sys.argv[2])
-elif sys.argv[1] == "set":
+elif sys.argv[1] == "set" and len(sys.argv) > 2:
     snap.setConfig(sys.argv[2:])
 elif sys.argv[1] == "help":
-    print("update, create, list, download, delete, restore, set <key> <value>")
+    print("update, create <container>, list <container>, download <fid>, delete <fid>, restore <container>, set <key> <value>")
+else:
+    print("Missing argument, try help")
